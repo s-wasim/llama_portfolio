1ce32e1eb4125f253e8c797d73164d39896571b2
s-wasim <s.w150700@hotmail.com>
2025-01-27 22:31:28 +0500

    Modified application to work better with specific context

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from commons.index import Index
-
+"""
 app = Flask(__name__)
 index = Index('init', 'portfolio_documents')
 
@@ -13,15 +13,19 @@ def query_portfolio():
     question = request.args.get('question', '')
     response = index(question)
     return jsonify({"response": response})
-
+"""
 def main():
     index = Index('init', 'portfolio_documents')
     send_msg = ''
-    while send_msg != '/exit':
+    while True:
         send_msg = input('Enter your message: ')
+        if send_msg == '/exit':
+            print('Bye bye')
+            break
         resp = index(send_msg)
-        print(resp)
+        print(resp.response)
     
 if __name__ == '__main__':
+
     main()
     
