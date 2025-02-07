15b32ca47514ff3a8df6d4a29b8b7663acaaa121
s-wasim <104296762+s-wasim@users.noreply.github.com>
2025-02-07 22:52:32 +0500

    Update main.py
    Fixed main to work more efficiently

--- main.py
+++ main.py
@@ -5,18 +5,26 @@ class CommandError(Exception):
     def __init__(self, msg='Incorrect!'):
         self.mssg=msg
 
-def main():
-    if len(sys.argv) > 1:
-        if sys.argv[1] != '-cm' or sys.argv[0] != '--chat-mode':
-            raise CommandError('Wrong parameter used')
-        if sys.argv[0].lower() not in ['interactive', 'default']:
-            raise CommandError('Incorrect parameter. Can be one of [interactive OR default]')
-    index = Index('init', 'portfolio_documents')
+def load():
+    return Index('init', 'portfolio_documents')
+def test(index):
     while True:
         mssg = input('Enter Message: ').strip()
+        if mssg.strip().lower() == '\exit':
+            break
         response = index(mssg=mssg)
         print('\t', response)
         print(f'\n{"".join(["-"] * 70)}', end='\n\n')
+
+def main():
+    if len(sys.argv) != 1:
+        raise CommandError('Wrong number of parameters supplied')
+    if sys.argv[1] == '--test':
+        test(load())
+    elif sys.argv[1] == '--load':
+        load()
+    else:
+        raise CommandError('Incorrect Parameters supplied')
 
 if __name__ == '__main__':
     main()
